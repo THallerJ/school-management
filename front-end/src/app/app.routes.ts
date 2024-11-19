e0e567920af42d0aeb6e11f8ac9a2d1f6b63370e
Thomas Haller <hallert60@gmal.com>
2024-11-19 17:14:28 -0600

    started working on router

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { StudentsComponent } from "./students/students.component";
+import { TeachersComponent } from "./teachers/teachers.component";
+import { CoursesComponent } from "./courses/courses.component";
+import { SchoolsComponent } from "./schools/schools.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: "students", component: StudentsComponent },
+	{ path: "teachers", component: TeachersComponent },
+	{ path: "courses", component: CoursesComponent },
+	{ path: "schools", component: SchoolsComponent },
+];
