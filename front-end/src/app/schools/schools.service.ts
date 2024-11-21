ab7976bffac68f06a5a87eac9eef350327ebfc6c
Thomas Haller <hallert60@gmal.com>
2024-11-20 21:57:08 -0600

    added filter to getSchools

--- front-end/src/app/schools/schools.service.ts
+++ front-end/src/app/schools/schools.service.ts
@@ -10,8 +10,15 @@ export class SchoolsService {
 	private apiUrl = `${environment.apiUrl}/school`;
 	http = inject(HttpClient);
 
-	getSchools(): Observable<any> {
-		const params = {};
+	getSchools(params?: GetSchoolFilter): Observable<any> {
 		return this.http.get(this.apiUrl, { params });
 	}
 }
+
+type GetSchoolFilter = {
+	pageNumber?: number;
+	pageSize?: number;
+	name?: string;
+	address?: string;
+	phoneNumber?: string;
+};
