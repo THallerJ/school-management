8abff9adc98ef1c717560005d55ba77a5ea1b1d8
Thomas Haller <hallert60@gmal.com>
2024-11-22 23:22:44 -0600

    extracted header to its own component

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
-import { RouterOutlet, RouterLink } from "@angular/router";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./core/components/header/header.component";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [RouterOutlet, RouterLink],
+	imports: [RouterOutlet, HeaderComponent],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.css",
 })
