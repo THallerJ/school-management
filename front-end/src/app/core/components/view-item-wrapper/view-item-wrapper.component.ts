33f294f870ae9ee08bd8be619367927c36b7e13c
Thomas Haller <hallert60@gmal.com>
2024-11-30 18:56:15 -0600

    added spinner

--- front-end/src/app/core/components/view-item-wrapper/view-item-wrapper.component.ts
+++ front-end/src/app/core/components/view-item-wrapper/view-item-wrapper.component.ts
@@ -1,11 +1,12 @@
 import { ModalService } from "./../../services/modal-service.service";
 import { Component, Input } from "@angular/core";
 import { ConfirmationModalComponent } from "../confirmation-modal/confirmation-modal.component";
+import { SpinnerComponent } from "../spinner/spinner.component";
 
 @Component({
 	selector: "app-view-item-wrapper",
 	standalone: true,
-	imports: [ConfirmationModalComponent],
+	imports: [ConfirmationModalComponent, SpinnerComponent],
 	templateUrl: "./view-item-wrapper.component.html",
 	styleUrl: "./view-item-wrapper.component.css",
 })
