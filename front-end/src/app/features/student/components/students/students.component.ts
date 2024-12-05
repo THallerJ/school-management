25afee2ac1bf096692c7fa896e0dc03ca906130e
Thomas Haller <hallert60@gmal.com>
2024-12-04 22:50:30 -0600

    made progress on students

--- front-end/src/app/features/student/components/students/students.component.ts
+++ front-end/src/app/features/student/components/students/students.component.ts
@@ -1,18 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../../core/services/api-service.service';
 import { StudentDtoRespSchema, StudentDtoResp } from '../../../../core/types';
+import { Router } from '@angular/router';
+import { ConditionalMessageComponent } from './../../../../core/components/conditional-message/conditional-message.component';
+import { ApiContentWrapperComponent } from '../../../../core/components/api-content-wrapper/api-content-wrapper.component';
+import { ListItemComponent } from '../../../../core/components/list-item/list-item.component';
+import { ItemListHeaderComponent } from '../../../../core/components/item-list-header/item-list-header.component';
 
 @Component({
     selector: 'app-students',
+    standalone: true,
     templateUrl: './students.component.html',
     styleUrls: ['./students.component.css'],
+    imports: [
+        ConditionalMessageComponent,
+        ApiContentWrapperComponent,
+        ListItemComponent,
+        ItemListHeaderComponent,
+    ],
 })
 export class StudentsComponent implements OnInit {
     private readonly PATH = 'student';
+    loading = true;
+    readonly PAGE_SIZE = 20;
+    students?: StudentDtoResp;
 
-    students: StudentDtoResp = [];
-
-    constructor(private apiService: ApiService) {}
+    constructor(
+        private apiService: ApiService,
+        private router: Router,
+    ) {}
 
     ngOnInit(): void {
         this.getStudents();
@@ -23,8 +39,11 @@ export class StudentsComponent implements OnInit {
             next: data => {
                 const result = StudentDtoRespSchema.safeParse(data);
                 if (result.success) this.students = result.data;
+                this.loading = false;
             },
             error: error => {
+                this.loading = false;
+
                 if (error.status === 0) {
                     return;
                 }
